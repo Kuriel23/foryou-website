400e1175d5c2d186ca379db98ede462f1947fcdb
izakdvlpr <izakdvlpr@gmail.com>
2022-12-05 15:25:31 -0300

    feat: create next auth config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,27 @@
 /**
  * FONTS
  *
- * https://fontsource.org/fonts/m-plus-2
- * https://fontsource.org/fonts/figtree
+ * https://fontsource.org/fonts/poppins
  */
 
-import '@fontsource/m-plus-2';
-import '@fontsource/figtree';
+import '@fontsource/poppins';
 
 import { ChakraProvider } from '@chakra-ui/react';
+import { SessionProvider } from 'next-auth/react';
 import type { AppProps } from 'next/app';
 
 import { theme } from '@theme';
 
-export default function App({ Component, pageProps }: AppProps): JSX.Element {
+export default function App({
+  Component,
+  session,
+  pageProps,
+}: AppProps & Record<'session', any>): JSX.Element {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <SessionProvider session={session}>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </SessionProvider>
   );
 }
