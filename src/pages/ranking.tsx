e9e02739e88c9b00953b68526989abeaef9cf710
Kuriel <37979213+ipaddysz@users.noreply.github.com>
2022-12-07 12:58:04 +0000

    Feat: starting ranking page

---
+++ src/pages/ranking.tsx
@@ -0,0 +1,9 @@
+import { DefaultLayout } from '@components/templates/DefaultLayout';
+
+export default function Ranking(): JSX.Element {
+  return (
+    <DefaultLayout>
+
+    </DefaultLayout>
+  );
+}
