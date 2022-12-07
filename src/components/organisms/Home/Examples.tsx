fb2c4d3804a0e6600b467917172a97ba00d45d38
Kuriel <37979213+ipaddysz@users.noreply.github.com>
2022-12-07 23:06:27 +0000

    refactor: text for examples

--- src/components/organisms/Home/Examples.tsx
+++ src/components/organisms/Home/Examples.tsx
@@ -25,28 +25,28 @@ export function Examples(): JSX.Element {
           textAlign="center"
         >
           <Heading as="h2" size="xl" color={theme.textColor}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Quer ver um pouco das minhas funções?
           </Heading>
 
           <Text maxW="800px" color={theme.textColor}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
-            quis tenetur nobis sapiente hic, blanditiis corrupti quidem esse
-            libero obcaecati laudantium quam culpa, eveniet, in adipisci eos
-            aperiam. Odit, mollitia.
+            Deixa eu te dar o cheirinho de algumas das minhas funções através de
+            imagens e explicando melhor elas por texto, que tal? Se você descer
+            um pouco você encontrará algumas imagens dos meus comandos feitos
+            pelos meus desenvolvedores.
           </Text>
         </Flex>
 
         <Flex flexDir="column" w="100%" gap={8}>
           <Flex w="100%" p={12} align="center" gap={12}>
             <Img
-              src="/images/examples/example_ping.png"
+              src="/images/examples/entretenimento.png"
               alt="blob"
               maxHeight="400px"
               borderRadius={12}
             />
 
             <Flex flexDir="column" gap={4}>
-              <Badge w="46px">Music</Badge>
+              <Badge maxW="max-content">Entretenimento</Badge>
 
               <Heading
                 as="h3"
@@ -54,21 +54,21 @@ export function Examples(): JSX.Element {
                 color={theme.textColor}
                 sx={{ span: { color: 'green.300' } }}
               >
-                Quam culpa, eveniet in adipisci.
+                O <span>entretenimento</span> é tudo, ainda mais com fofura
               </Heading>
 
               <Text color={theme.textColor}>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
-                commodi repellat error alias dicta aut, nam eligendi cupiditate
-                dolorem placeat vel hic in distinctio ducimus nesciunt, debitis
-                architecto maxime laborum!
+                Você não tem um gato nem um cachorro? Sem problemas, seja mimado
+                por esses bixinhos fofos através dos comandos de gato ou de
+                cachorro. Ou se sente carente? Abrace alguém virtualmente, com o
+                comando de abraço.
               </Text>
             </Flex>
           </Flex>
 
           <Flex w="100%" p={12} align="center" gap={12}>
             <Flex flexDir="column" gap={4}>
-              <Badge w="46px">Music</Badge>
+              <Badge maxW="max-content">Economia</Badge>
 
               <Heading
                 as="h3"
@@ -76,19 +76,19 @@ export function Examples(): JSX.Element {
                 color={theme.textColor}
                 sx={{ span: { color: 'green.300' } }}
               >
-                Quam culpa, eveniet in adipisci.
+                Ser rico em <span>animecoins</span>, claro!
               </Heading>
 
               <Text color={theme.textColor}>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
-                commodi repellat error alias dicta aut, nam eligendi cupiditate
-                dolorem placeat vel hic in distinctio ducimus nesciunt, debitis
-                architecto maxime laborum!
+                Seja viciado em roletas, dar reputações, e ficar sempre olhando
+                o seu banco, pegando o daily, crime, pegar alguns fundos novos
+                para o seu perfil e muito mais apenas com simples comandos porém
+                que você facilmente é atraído ao ponto de serem viciantes.
               </Text>
             </Flex>
 
             <Img
-              src="/images/examples/example_help.png"
+              src="/images/examples/economia.png"
               alt="blob"
               maxHeight="500px"
               borderRadius={12}
@@ -97,14 +97,14 @@ export function Examples(): JSX.Element {
 
           <Flex w="100%" p={12} align="center" gap={12}>
             <Img
-              src="/images/examples/example_ping.png"
+              src="/images/examples/informacao.png"
               alt="blob"
               maxHeight="400px"
               borderRadius={12}
             />
 
             <Flex flexDir="column" gap={4}>
-              <Badge w="46px">Music</Badge>
+              <Badge maxW="max-content">Informação</Badge>
 
               <Heading
                 as="h3"
@@ -112,14 +112,17 @@ export function Examples(): JSX.Element {
                 color={theme.textColor}
                 sx={{ span: { color: 'green.300' } }}
               >
-                Quam culpa, eveniet in adipisci.
+                A <span>informação</span> é importante, principalmente para
+                Otakus
               </Heading>
 
               <Text color={theme.textColor}>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
-                commodi repellat error alias dicta aut, nam eligendi cupiditate
-                dolorem placeat vel hic in distinctio ducimus nesciunt, debitis
-                architecto maxime laborum!
+                A informação é a base do mundo, sem ela seria quase impossível
+                de sobreviver neste mundo informado, principalmente as novas
+                tecnologias facilitam o recebimento de informação. Mas você é um
+                otaku, logo gosta de ficar informado quanto a comandos como o de
+                anime nos quais informam episódios, status, gêneros e até
+                informa se está disponível no website do Animes Online Games.
               </Text>
             </Flex>
           </Flex>
