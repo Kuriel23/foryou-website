4d976fb7a63460a4ede25d8b9f1b503bf5c2afa2
Kuriel <37979213+ipaddysz@users.noreply.github.com>
2022-12-07 16:16:47 +0000

    refactor: new banner import

--- src/components/organisms/Profile/index.tsx
+++ src/components/organisms/Profile/index.tsx
@@ -2,10 +2,12 @@ import { Flex } from '@chakra-ui/react';
 
 import { Information } from './Information';
 import { Married } from './Married';
+import { Banner } from './Banner';
 
 export function Profile(): JSX.Element {
   return (
     <Flex flexDir="column" gap={6}>
+      <Banner />
       <Information />
       <Married />
     </Flex>
