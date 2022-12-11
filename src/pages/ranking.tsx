15f8d286e811365a058a760eb71897a017c5c278
izakdvlpr <izakdvlpr@gmail.com>
2022-12-11 14:10:51 -0300

    refactor: temporarily remove page

--- src/pages/ranking.tsx
+++ src/pages/ranking.tsx
@@ -1,51 +1,10 @@
-import type { GetStaticProps } from 'next';
-
 import { SEO } from '@components/atoms/SEO';
-import { Ranking } from '@components/organisms/Ranking';
 import { DefaultLayout } from '@components/templates/DefaultLayout';
-import { RankingProvider } from '@contexts/RankingContext';
-import {
-  getUsersRanking,
-  type GetUsersRanking,
-} from '@services/getUsersRanking';
 
-interface RankingPageProps {
-  usersCoinsRanking: GetUsersRanking;
-  usersLevelRanking: GetUsersRanking;
-  usersRepRanking: GetUsersRanking;
-}
-
-export const getStaticProps: GetStaticProps = async () => {
-  const usersCoinsRanking = await getUsersRanking('coins');
-  const usersLevelRanking = await getUsersRanking('level');
-  const usersRepRanking = await getUsersRanking('rep');
-
-  return {
-    props: {
-      usersCoinsRanking,
-      usersLevelRanking,
-      usersRepRanking,
-    },
-    revalidate: 60,
-  };
-};
-
-export default function RankingPage({
-  usersCoinsRanking,
-  usersLevelRanking,
-  usersRepRanking,
-}: RankingPageProps): JSX.Element {
+export default function RankingPage(): JSX.Element {
   return (
     <DefaultLayout>
       <SEO />
-
-      <RankingProvider>
-        <Ranking
-          usersCoinsRanking={usersCoinsRanking}
-          usersLevelRanking={usersLevelRanking}
-          usersRepRanking={usersRepRanking}
-        />
-      </RankingProvider>
     </DefaultLayout>
   );
 }
