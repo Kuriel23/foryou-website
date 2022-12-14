4806e0dc45b891e14b66e73e44647dc5ea216ba6
izakdvlpr <izakdvlpr@gmail.com>
2022-12-14 20:47:27 -0300

    feat: create NotFound component

---
+++ src/components/organisms/NotFound/index.tsx
@@ -0,0 +1,77 @@
+import {
+  Flex,
+  Box,
+  Img,
+  Heading,
+  Text,
+  Button,
+  useColorModeValue,
+} from '@chakra-ui/react';
+import { useRouter } from 'next/router';
+
+export function NotFound(): JSX.Element {
+  const router = useRouter();
+
+  const theme = {
+    containerBackground: useColorModeValue('gray.100', 'gray.700'),
+    textColor: useColorModeValue('gray.700', 'white'),
+  };
+
+  return (
+    <Flex
+      bgImage="https://i.imgur.com/URGcI2z.jpeg"
+      bgRepeat="no-repeat"
+      bgSize="cover"
+      bgPos="center"
+      w="100vw"
+      h="100vh"
+    >
+      <Flex w="100vw" h="100vh" align="center" justify="center">
+        <Box
+          p={12}
+          w="60%"
+          bg={theme.containerBackground}
+          borderRadius={12}
+          textAlign="center"
+        >
+          <Img
+            src="/images/illustrations/undraw_by_the_road_re_vvs7.svg"
+            alt="blob"
+            w="400px"
+            m={{ base: '10px auto', xl: '50px auto' }}
+          />
+
+          <Heading
+            color={theme.textColor}
+            sx={{ span: { color: 'green.300' } }}
+            fontSize={{ base: '2xl', xl: '5xl' }}
+            mb="15px"
+          >
+            Isto parece ser <span>perigoso</span>!
+          </Heading>
+
+          <Text
+            color={theme.textColor}
+            m="15px auto"
+            maxW={{ base: '90%', xl: '60%' }}
+            fontSize={{ base: 'md', xl: 'xl' }}
+          >
+            Isto não estava previsto no meu mapa, parece que é melhor voltarmos
+            antes que encontremos alguns perigos dos quais não gostariamos de
+            encarar.
+          </Text>
+
+          <Button
+            borderRadius={12}
+            bgColor="green.200"
+            color="gray.700"
+            fontSize={{ base: 'md', xl: 'xl' }}
+            onClick={() => router.push('/')}
+          >
+            Voltar a um lugar seguro
+          </Button>
+        </Box>
+      </Flex>
+    </Flex>
+  );
+}
