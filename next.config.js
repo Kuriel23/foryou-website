b43f940eb80851b4d74b5198b4c6095ce2532837
izakdvlpr <izakdvlpr@gmail.com>
2022-12-14 21:13:50 -0300

    chore: create profile redirect

--- next.config.js
+++ next.config.js
@@ -2,4 +2,13 @@
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: '/profile',
+        destination: '/',
+        permanent: false,
+      },
+    ];
+  },
 };
--- src/pages/profile/index.tsx
+++
@@ -1,9 +0,0 @@
-import { GetServerSideProps } from 'next';
-
-export default function ProfileIndex(): JSX.Element {
-  return <> </>;
-}
-
-export const getServerSideProps: GetServerSideProps = async () => {
-  return { redirect: { destination: '/', permanent: false } };
-};
